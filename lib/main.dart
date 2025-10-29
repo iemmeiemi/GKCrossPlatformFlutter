ff2f4ed39d9bdebf7152534ffec77dc025034c18
iemmeiemi <duoinuicholantu6x9@gmail.com>
2025-10-29 11:01:28 +0700

    set up project

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var db = FirebaseFirestore.instance;
+  final user = <String, dynamic>{
+    "first": "Ada",
+    "last": "Lovelace",
+    "born": 1815
+  };
+  var current = "null";
+  void addSample() {
+    db.collection("Users").add(user).then((DocumentReference doc) {
+      current = doc.id;
+      print('DocumentSnapshot added with ID: ${doc.id}');
+      notifyListeners();
+    }).catchError((error) {
+      print('🔥 Lỗi khi thêm user: $error');
+    });
+  }
+
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                appState.addSample();
+              },
+              child: Text('Add'),
+            ),
+            Text(appState.current),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+}
